fb8671b866c1e1aed683fb524bff8185dc04a49f
Alfonso <dcao201793@upemor.edu.mx>
2022-04-15 04:36:00 -0500

    Comienzo de Skills y creacion de Slide
    Se intento terminar el nav con la selección de seccion pero no se tuvo exito, ademas se comenzo a trabajar con la maquetación de Skills y el slide esta terminado pero no se puede modificar mucho el auto-width y se planea cambiarlo para usar Glide.js

--- DesarrolloWEB/JS/NavScrpt.js
+++ DesarrolloWEB/JS/NavScrpt.js
@@ -15,13 +15,11 @@ document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 menu = document.getElementById("header");
 body = document.getElementById("container_all");
 nav = document.getElementById("nav");
-/*footer = document.getElementById("footer");*/
 
 function mostrar_menu(){
     menu.classList.toggle('move_content');
     body.classList.toggle('move_content');
     nav.classList.toggle('move_nav');
-    /*footer.classList.toggle('move_content');*/
 }
 
 window.addEventListener("resize", function(){
@@ -29,6 +27,53 @@ window.addEventListener("resize", function(){
         menu.classList.remove('move_content');
         body.classList.remove('move_content');
         nav.classList.remove('move_nav');
-        /*footer.classList.toggle('move_content');*/
     }
 })
+
+/***********    SELECT MENU    ***********/
+/*
+const encabezados = document.querySelectorAll('.contenedor .encabezado');
+const enlaces = document.querySelectorAll('#enlaces a');
+
+const observer = new IntersectionObserver((entradas, observador) => {
+    entradas.forEach(entrada => {
+        if(entrada.isIntersecting){
+            const id = '#' + entrada.target.id;
+            history.pushState({}, entrada.target.innerText, id);
+
+            enlaces.forEach(enlace => {
+                enlace.classList.remove('activo');
+                
+                const href = enlace.attributes.href.nodeValue;
+
+                if(href === id){
+                    enlace.classList.add('activo');
+                }
+            });
+        }
+    });
+}, {
+    threshold: 1,
+    rootMargin: '0px 0px -50% 0px'
+});
+
+encabezados.forEach(encabezado => {
+    observer.observe(encabezado);
+});
+*/
+
+/***********    SLIDE   *************/
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+
+
+
